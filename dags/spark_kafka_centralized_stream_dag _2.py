7e6c67d78fb729205982bcde3ba29395a2b9ca75
tridtaytech <tridsanu.tay.tech@gmail.com>
2025-08-31 17:13:58 +0700

    update

--- dags/spark_kafka_centralized_stream_dag _2.py
+++ dags/spark_kafka_centralized_stream_dag _2.py
@@ -8,7 +8,7 @@ default_args = {
 }
 
 with DAG(
-    "spark_kafka_streaming_centralized",
+    "spark_kafka_streaming_centralized_2",
     default_args=default_args,
     start_date=datetime(2025, 8, 17),
     schedule=None,   # run on trigger
@@ -18,7 +18,7 @@ with DAG(
     # https://airflow.apache.org/docs/apache-airflow-providers-apache-spark/stable/_modules/airflow/providers/apache/spark/operators/spark_submit.html#SparkSubmitOperator.application
     start_stream = SparkSubmitOperator(
         application="/opt/airflow/dags/spark_kafka_centralized_stream.py",
-        task_id="start_kafka_stream_centralized",
+        task_id="start_kafka_stream_centralized_2",
         conn_id="spark_default",   # set in Airflow Connections
         name="centralized",
         packages= "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0",
